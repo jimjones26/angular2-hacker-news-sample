0357103c78280b3f23e675686fa977e78d76aa64
jimjones26 <jimjones26@gmail.com>
2016-09-26 21:15:02 -0400

    hook up API service and create layout and styling for news items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DomainPipe } from './domain.pipe';
+import { MomentModule } from 'angular2-moment';
 import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
@@ -17,12 +19,14 @@ import { HackerNewsAPIService } from './hackernews-api.service';
     HeaderComponent,
     StoriesComponent,
     FooterComponent,
-    ItemComponent
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule
   ],
   providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
