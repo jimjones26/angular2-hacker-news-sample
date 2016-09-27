1382f96ec89ed120e2871540cd95efed0e555bcf
jimjones26 <jimjones26@gmail.com>
2016-09-26 21:53:00 -0400

    fix ItemComments component not being found issue in console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
 
 import { HackerNewsAPIService } from './hackernews-api.service';
-import { ItemCommentsComponent } from './item-comments/item-comments.component';
 
 @NgModule({
   declarations: [
